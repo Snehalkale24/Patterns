66d555c77539376d4fc2c549f419a916f7e5b798
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-21 21:25:56 +0530

    add

--- ex1.java
+++ ex1.java
@@ -1,5 +1,17 @@
 public class ex1{
 	public static void main(String args[]){
-		
+		int n=5;
+		for(int i=1;i<=n;i++){
+			System.out.println();
+			for(int j=1;j<=n;j++)
+			{
+				System.out.print("*");
+			}
+		}
 	}
 }
+
+//*****
+//*****
+//*****
+//*****
