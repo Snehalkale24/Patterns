71d0cdef82f8a7919ba60dac0733ccb13060acc2
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-21 21:28:11 +0530

    add

---
+++ ex2.java
@@ -0,0 +1,14 @@
+public class ex2{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=n;i>=1;i--)
+		{
+			System.out.println();
+			for(int j=n;j>=1;j--)
+			{
+				System.out.print(i);
+			}
+		}
+	}
+}
